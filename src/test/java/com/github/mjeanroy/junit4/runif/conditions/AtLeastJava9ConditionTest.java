5d63b8200870babced69d8a7753a421fdaddaee9
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2026-03-28 17:51:55 +0100

    feat: add java 9 conditions

---
+++ src/test/java/com/github/mjeanroy/junit4/runif/conditions/AtLeastJava9ConditionTest.java
@@ -0,0 +1,87 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018-2026 Mickael Jeanroy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.junit4.runif.conditions;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AtLeastJava9ConditionTest {
+
+	@Test
+	public void it_should_return_true_with_true_with_java_17() {
+		JavaTestingUtils.withJavaSpecificationVersion("17", new Runnable() {
+				@Override
+				public void run() {
+					assertThat(new AtLeastJava9Condition().apply()).isTrue();
+				}
+			}
+		);
+	}
+
+	@Test
+	public void it_should_return_true_with_true_with_java_11() {
+		JavaTestingUtils.withJavaSpecificationVersion("11", new Runnable() {
+				@Override
+				public void run() {
+					assertThat(new AtLeastJava9Condition().apply()).isTrue();
+				}
+			}
+		);
+	}
+
+	@Test
+	public void it_should_return_true_with_true_with_java_9() {
+		JavaTestingUtils.withJavaSpecificationVersion("9", new Runnable() {
+				@Override
+				public void run() {
+					assertThat(new AtLeastJava9Condition().apply()).isTrue();
+				}
+			}
+		);
+	}
+
+	@Test
+	public void it_should_return_true_with_false_with_java_8() {
+		JavaTestingUtils.withJavaSpecificationVersion("1.8", new Runnable() {
+				@Override
+				public void run() {
+					assertThat(new AtLeastJava9Condition().apply()).isFalse();
+				}
+			}
+		);
+	}
+
+	@Test
+	public void it_should_return_false_with_java_7() {
+		JavaTestingUtils.withJavaSpecificationVersion("1.7", new Runnable() {
+				@Override
+				public void run() {
+					assertThat(new AtLeastJava9Condition().apply()).isFalse();
+				}
+			}
+		);
+	}
+}
